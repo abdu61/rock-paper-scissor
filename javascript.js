9e63c992efd13ca1ad8495b1ed3698457e939c90
abdu61 <112977047+abdu61@users.noreply.github.com>
2023-10-17 22:14:09 +0300

    Added playRound() in Javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function getComputerChoice( ){
+function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3);
     switch(randomNumber){
         case 0:
@@ -10,9 +10,9 @@ function getComputerChoice( ){
     }
 }
 
+
 function playerSelection() {
-    let playerChoice 
-    prompt("Choose rock, paper or scissors");
+    let playerChoice = prompt("Choose rock, paper or scissors");
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors"){
         return playerChoice;
@@ -22,5 +22,60 @@ function playerSelection() {
         playerSelection();
     }
 }
+
+
+function playRound(player, computer) {
+    if(computer === "rock")
+    {
+        if(player === "scissors")
+        {
+            return "You lose! Rock beats scissors";
+        }
+        else if(player === "paper")
+        {
+            return "You win! Paper beats rock";
+        }
+        else
+        {
+            return "It's a tie!";
+        }
+    }   
+    else if(computer === "paper")
+    {
+        if(player === "rock")
+        {
+            return "You lose! Paper beats rock";
+        }
+        else if(player === "scissors")
+        {
+            return "You win! Scissors beats paper";
+        }
+        else
+        {
+            return "It's a tie!";
+        }
+    }
+    else 
+    {
+        if(player === "paper")
+        {
+            return "You lose! Scissors beats paper";
+        }
+        else if(player === "rock")
+        {
+            return "You win! Rock beats scissors";
+        }
+        else
+        {
+            return "It's a tie!";
+        }
+    }
+    
+}
+
+
+let comp = getComputerChoice();
+console.log(comp);
+console.log(playRound(playerSelection(), comp));
 
 
